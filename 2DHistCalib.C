92ae612aad24230259529dba58ebe6a2165c5836
Carl Unsworth <carlunsworthuk@gmail.com>
2014-06-19 18:29:30 -0700

    2DHistCalib.C:CorrelateSegsCores() now loops and displays CoreChg-SegChg matrices from input file.

--- 2DHistCalib.C
+++ 2DHistCalib.C
@@ -16,7 +16,6 @@ using namespace std;
 #include <TTreeIndex.h>
 #include <TTreePlayer.h>
 #include <TChain.h>
-//#include <TVector3.h>
 #include <TH1F.h>
 #include <TH2F.h>
 #include <TF1.h>
@@ -28,9 +27,6 @@ using namespace std;
 #include <TFolder.h>
 #include <TRandom3.h>
 
-// TriScope libraries
-//#include "TTigFragment.h"
-
 // My libraries
 #include "Options.h"
 #include "SortTrees.h"
@@ -38,6 +34,11 @@ using namespace std;
 #include "HistCalib.h"
 #include "2DHistCalib.h"
 #include "Utils.h"
+
+extern TApplication *App;
+
+// globals
+TCanvas *cCalib = NULL;
 
 
 int CorrelateSegsCores() {
@@ -48,7 +49,8 @@ int CorrelateSegsCores() {
    int Seg = 0;
    std::string Filename;
    TFile *File = NULL;
-   
+   char CharBuf[CHAR_BUFFER_SIZE];
+   TH1F *Histo = NULL;
    // Check inputs/configuration
    if(Config.files.size() != 1) {
       cout << "One file expected for seg calibration by seg-core correlation." << endl;
@@ -70,7 +72,27 @@ int CorrelateSegsCores() {
       return 1;
    }
    
+   // Set up TCanvas
+   cCalib = new TCanvas("cCalib", "2D Calib", 800, 600);
       
+   // Now file should be open, loop Cl,Cr,Seg and load histos
+   for(Clover=1; Clover <= CLOVERS; Clover++) {
+      for(Crystal = 0; Crystal < CRYSTALS; Crystal++) {
+         for(Seg=1; Seg<=SEGS; Seg++) { // should looop segs but not cores
+            Histo = NULL;
+            snprintf(CharBuf,CHAR_BUFFER_SIZE,"TIG%02d%cP%02dx Chg Mat",Clover,Num2Col(Crystal),Seg);
+            cout << CharBuf << endl;
+            Histo = (TH1F*) File->FindObjectAny(CharBuf);
+            if(Histo) {
+               cCalib->cd();
+               Histo->Draw("colz");
+               App->Run(1);
+            }
+            
+         }
+      }
+   }
+   
    
    File->Close();
 
